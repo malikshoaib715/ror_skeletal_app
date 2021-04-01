58dee239ee3df7a7f5e466061e954368253f5e77
malikshoaib715 <malik.shoaib715@gmail.com>
2021-04-01 13:47:47 +0500

    Actiona mailer perform deliveried in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { :host => ' herokuapp.com'}
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -118,7 +121,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.delivery_method = :smtp
   #host = 'gmail.com'
   config.action_mailer.smtp_settings = {
     port: 587,
