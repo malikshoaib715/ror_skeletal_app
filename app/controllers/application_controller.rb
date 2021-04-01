c155f91e36bbb05191f862194064557901e4e8c3
malikshoaib715 <malik.shoaib715@gmail.com>
2021-04-01 17:12:38 +0500

    checking

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   #before_action :authenticate_user!
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, :alert => exception.message
-  end
+  # rescue_from CanCan::AccessDenied do |exception|
+  #   redirect_to '/', :alert => exception.message
+  # end
   protected
     def after_sign_in_path_for(resource)
       home_index_path
