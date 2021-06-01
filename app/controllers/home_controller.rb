e59cb389987d3bb1914a77173e669d68f543dcd1
malikshoaib715 <malik.shoaib715@gmail.com>
2021-06-01 16:44:44 +0500

    adding pie and bar chart

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -17,14 +17,25 @@ class HomeController < ApplicationController
   def superadmin_home; end
 
   def supervisor_home
+
+  end
+
+  def supervisor_home_reports
     @reports = Report.all
+
     respond_to do |format|
       format.js {render 'home/reports.js.erb'} # Add this line to you respond_to block
       format.html
       format.csv {send_data Report.to_csv, filename: "report.csv"}
       format.xls {send_data Report.to_csv, filename: "report.xls"}
     end
+  end
 
+  def supervisor_home_charts
+    @data = Report.group(:status).count
+    respond_to do |format|
+      format.js {render 'home/charts.js.erb'}
+    end
 
   end
 
